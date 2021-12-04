5022a320924b059dcb090993731352f0d8dbc2b4
Jakub Luczak <jakubluczak90@gmail.com>
2021-12-04 13:53:14 +0100

    Update script.js

--- task1-browser/script.js
+++ task1-browser/script.js
@@ -12,16 +12,31 @@ const getFiguresData=()=>{
     return (figuresData.sort((a,b)=>a-b));
 }
 const reset=()=>{
+    // w funkcji reset po prostu usuń elementy z DOMu;
     figuresData=[]
     evenDataHTML=''
     oddDataHTML=''
 }
 const loadTable=(figuresData)=>{
+    // Zamiast używać i modyfikować figuresData na górze dużo lepiej byłoby aby funkcja to zwracała
+    // const figuresData = getFiguresData();
     getFiguresData()
+    
+    // Masz na górze już tables, więc możesz zrobić tables.getElementById('evenTableData');
     const evenTableBody=document.getElementById('evenTableData');
     const oddTableBody=document.getElementById('oddTableData')
  
     
+    // Ponownie - przenieś evenDataHtml i oddDataHtml tutaj, nie używaj globalnych zmiennych.
+    /*
+        Dodatkowo zamiast operować na stringach dużo lepiej byłoby tworzyć elementy.
+        const evenDataHtml = document.createElement('tr');
+        
+        const td = document.createElement('td');
+        td.innerHTML = data;
+        
+        evenData.appendChild(td)
+    */
     for(let data of figuresData){
         if(data % 2 == 0){
             evenDataHTML += `<tr><td>${data}</td></tr>`}
@@ -30,7 +45,15 @@ const loadTable=(figuresData)=>{
         }
         tables.forEach((table)=>table.className='active')   
         evenTableBody.innerHTML=evenDataHTML;
-        oddTableBody.innerHTML=oddDataHTML  ; 
+        oddTableBody.innerHTML=oddDataHTML;
+    
+        // funkcję reset przerzuć do click listenera
+        /*
+        btn.addEventListener('click', ()=>{
+            resetTables();
+            loadTable();
+        });
+        */
         reset()
 
 }
@@ -38,5 +61,4 @@ const loadTable=(figuresData)=>{
 btn.addEventListener('click', ()=>{
     loadTable(figuresData)
     
-}
-)
+});
